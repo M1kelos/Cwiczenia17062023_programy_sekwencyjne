cbc6094f67bc150fbd33c973fe274d4c4f6c27f6
M1kelos <128227021+M1kelos@users.noreply.github.com>
2023-06-17 21:40:49 +0200

    Add files via upload

---
+++ 05_program_potegowanie.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = WprowadzLiczbeCalkowitaNieujemna("Podaj wykładnik (n): ");
+        int x = WprowadzLiczbeNaturalna("Podaj podstawę (x): ");
+
+        long wynik = ObliczPotegowanie(x, n);
+        long wynikBiblioteka = (long)Math.Pow(x, n);
+
+        Console.WriteLine("Wynik otrzymany algorytmem: " + wynik);
+        Console.WriteLine("Wynik prawidłowy (funkcja biblioteczna): " + wynikBiblioteka);
+
+        if (wynik == wynikBiblioteka)
+        {
+            Console.WriteLine("Wyniki są zgodne.");
+        }
+        else
+        {
+            Console.WriteLine("Wyniki nie są zgodne.");
+        }
+    }
+
+    static int WprowadzLiczbeCalkowitaNieujemna(string prompt)
+    {
+        int liczba;
+        bool poprawna = false;
+
+        do
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out liczba) && liczba >= 0)
+            {
+                poprawna = true;
+            }
+            else
+            {
+                Console.WriteLine("Błędne dane. Podaj poprawną liczbę całkowitą nieujemną.");
+            }
+        } while (!poprawna);
+
+        return liczba;
+    }
+
+    static int WprowadzLiczbeNaturalna(string prompt)
+    {
+        int liczba;
+        bool poprawna = false;
+
+        do
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out liczba) && liczba > 0)
+            {
+                poprawna = true;
+            }
+            else
+            {
+                Console.WriteLine("Błędne dane. Podaj poprawną liczbę naturalną.");
+            }
+        } while (!poprawna);
+
+        return liczba;
+    }
+
+    static long ObliczPotegowanie(int x, int n)
+    {
+        long wynik = x;
+        int poprzedniWykladnik = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            int aktualnyWykladnik = (i % 2 == 1) ? (i / 2) + 1 : -(i / 2);
+
+            int liczbaDodawan = x * (aktualnyWykladnik - poprzedniWykladnik);
+
+            wynik += liczbaDodawan;
+
+            poprzedniWykladnik = aktualnyWykladnik;
+        }
+
+        return wynik;
+    }
+}
